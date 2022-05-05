9ad1d2d369cbffeb421feeed431b00b1e7f8684b
jahongirbotirov <jahongirbotirov51@gmail.com>
2022-05-05 19:13:33 +0500

    First commit - certain image download

---
+++ app/src/main/java/com/example/downloadimage/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.downloadimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private String urlImage = "https://tinypng.com/images/social/website.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void onClickDownloadImage(View view) {
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(urlImage).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String,Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+}
